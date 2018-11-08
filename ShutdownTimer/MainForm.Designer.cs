e10246012f0d73a7d41598c9d6d60b0a0002a197
Faishal Ahammad <30669559+ahammadfaishal@users.noreply.github.com>
2018-11-09 01:16:33 +0600

    initial commit

---
+++ ShutdownTimer/MainForm.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace ShutdownTimer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.insertShutdownTimeInMinuteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dontHesitateToTellYourProblemsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.version1001ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.developerFaisalALMahmudIDBBISEWITScholarshipProjectRound25ESADCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.darkenergeeLIVEcomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facebookcomdarkenergeeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thisIsAOpenSourceSoftwareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hourLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.minuteLabel = new System.Windows.Forms.Label();
+            this.secondLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.Location = new System.Drawing.Point(26, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Shutdown";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Time";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Abort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(199, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(51, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(297, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.insertShutdownTimeInMinuteToolStripMenuItem,
+            this.dontHesitateToTellYourProblemsToolStripMenuItem,
+            this.version1001ToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // insertShutdownTimeInMinuteToolStripMenuItem
+            // 
+            this.insertShutdownTimeInMinuteToolStripMenuItem.Name = "insertShutdownTimeInMinuteToolStripMenuItem";
+            this.insertShutdownTimeInMinuteToolStripMenuItem.Size = new System.Drawing.Size(275, 22);
+            this.insertShutdownTimeInMinuteToolStripMenuItem.Text = "Insert shutdown time in minute.";
+            // 
+            // dontHesitateToTellYourProblemsToolStripMenuItem
+            // 
+            this.dontHesitateToTellYourProblemsToolStripMenuItem.Name = "dontHesitateToTellYourProblemsToolStripMenuItem";
+            this.dontHesitateToTellYourProblemsToolStripMenuItem.Size = new System.Drawing.Size(275, 22);
+            this.dontHesitateToTellYourProblemsToolStripMenuItem.Text = "Don\'t hesitate to share your problems.";
+            // 
+            // version1001ToolStripMenuItem
+            // 
+            this.version1001ToolStripMenuItem.Name = "version1001ToolStripMenuItem";
+            this.version1001ToolStripMenuItem.Size = new System.Drawing.Size(275, 22);
+            this.version1001ToolStripMenuItem.Text = "Version: 2.0.1.2";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.developerFaisalALMahmudIDBBISEWITScholarshipProjectRound25ESADCToolStripMenuItem,
+            this.darkenergeeLIVEcomToolStripMenuItem,
+            this.facebookcomdarkenergeeToolStripMenuItem,
+            this.thisIsAOpenSourceSoftwareToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // developerFaisalALMahmudIDBBISEWITScholarshipProjectRound25ESADCToolStripMenuItem
+            // 
+            this.developerFaisalALMahmudIDBBISEWITScholarshipProjectRound25ESADCToolStripMenuItem.Name = "developerFaisalALMahmudIDBBISEWITScholarshipProjectRound25ESADCToolStripMenuItem";
+            this.developerFaisalALMahmudIDBBISEWITScholarshipProjectRound25ESADCToolStripMenuItem.Size = new System.Drawing.Size(614, 22);
+            this.developerFaisalALMahmudIDBBISEWITScholarshipProjectRound25ESADCToolStripMenuItem.Text = "Developer: Faisal AL Mahmud, IDB-BISEW IT Scholarship Project, Round 25, ESAD-C#";
+            // 
+            // darkenergeeLIVEcomToolStripMenuItem
+            // 
+            this.darkenergeeLIVEcomToolStripMenuItem.Name = "darkenergeeLIVEcomToolStripMenuItem";
+            this.darkenergeeLIVEcomToolStripMenuItem.Size = new System.Drawing.Size(614, 22);
+            this.darkenergeeLIVEcomToolStripMenuItem.Text = "dark.energee@LIVE.com";
+            // 
+            // facebookcomdarkenergeeToolStripMenuItem
+            // 
+            this.facebookcomdarkenergeeToolStripMenuItem.Name = "facebookcomdarkenergeeToolStripMenuItem";
+            this.facebookcomdarkenergeeToolStripMenuItem.Size = new System.Drawing.Size(614, 22);
+            this.facebookcomdarkenergeeToolStripMenuItem.Text = "facebook.com/dark.energee";
+            // 
+            // thisIsAOpenSourceSoftwareToolStripMenuItem
+            // 
+            this.thisIsAOpenSourceSoftwareToolStripMenuItem.Name = "thisIsAOpenSourceSoftwareToolStripMenuItem";
+            this.thisIsAOpenSourceSoftwareToolStripMenuItem.Size = new System.Drawing.Size(614, 22);
+            this.thisIsAOpenSourceSoftwareToolStripMenuItem.Text = "Copyright: This is a Open Source Software. You can copy, edit and distribute it w" +
+    "ithout any permission.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.label2.Location = new System.Drawing.Point(151, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // hourLabel
+            // 
+            this.hourLabel.AutoSize = true;
+            this.hourLabel.BackColor = System.Drawing.Color.Transparent;
+            this.hourLabel.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hourLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.hourLabel.ImageKey = "(none)";
+            this.hourLabel.Location = new System.Drawing.Point(67, 237);
+            this.hourLabel.Name = "hourLabel";
+            this.hourLabel.Size = new System.Drawing.Size(0, 37);
+            this.hourLabel.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // minuteLabel
+            // 
+            this.minuteLabel.AutoSize = true;
+            this.minuteLabel.BackColor = System.Drawing.Color.Transparent;
+            this.minuteLabel.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minuteLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.minuteLabel.ImageKey = "(none)";
+            this.minuteLabel.Location = new System.Drawing.Point(118, 237);
+            this.minuteLabel.Name = "minuteLabel";
+            this.minuteLabel.Size = new System.Drawing.Size(0, 37);
+            this.minuteLabel.TabIndex = 8;
+            // 
+            // secondLabel
+            // 
+            this.secondLabel.AutoSize = true;
+            this.secondLabel.BackColor = System.Drawing.Color.Transparent;
+            this.secondLabel.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.secondLabel.ImageKey = "(none)";
+            this.secondLabel.Location = new System.Drawing.Point(170, 237);
+            this.secondLabel.Name = "secondLabel";
+            this.secondLabel.Size = new System.Drawing.Size(0, 37);
+            this.secondLabel.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = global::ShutdownTimer.Properties.Resources.Anonymous_Mirage;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(297, 284);
+            this.Controls.Add(this.secondLabel);
+            this.Controls.Add(this.minuteLabel);
+            this.Controls.Add(this.hourLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.HelpButton = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Shutdown Timer";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insertShutdownTimeInMinuteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dontHesitateToTellYourProblemsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem developerFaisalALMahmudIDBBISEWITScholarshipProjectRound25ESADCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem darkenergeeLIVEcomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem version1001ToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem facebookcomdarkenergeeToolStripMenuItem;
+        private System.Windows.Forms.Label hourLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label minuteLabel;
+        private System.Windows.Forms.Label secondLabel;
+        private System.Windows.Forms.ToolStripMenuItem thisIsAOpenSourceSoftwareToolStripMenuItem;
+    }
+}
+
---
+++ ShutdownTimer/MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ShutdownTimer
+{
+    public partial class MainForm : Form
+    {
+        int hh;
+        int s1;
+        int mm;
+        int s2;
+        Timer t = new Timer();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+        }
+
+        static void ExecuteCommand(string command)
+        {
+            int exit;
+            
+            ProcessStartInfo processInfo;
+            Process process;
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+            //process.WaitForExit();
+
+            // *** Read the streams ***
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            exit = process.ExitCode;
+
+            Console.WriteLine("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+            Console.WriteLine("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+            Console.WriteLine("ExitCode: " + exit.ToString(), "ExecuteCommand");
+            process.Close();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                int timer = Convert.ToInt32(textBox1.Text) * 60;
+                string time = Convert.ToString(timer);
+                timer1.Start();
+                ExecuteCommand("shutdown.exe /s /t " + time);
+                timeConverter();
+                textBox1.Clear();
+                button2.Enabled = true;
+                label2.Text = "";
+            }
+            catch 
+            {
+                label2.Text = "Please Insert Time";
+            }
+        }
+
+         public void timeConverter ()
+            {
+                int time = Convert.ToInt32(textBox1.Text) * 60;
+                hh = time / 3600;
+                s1 = time % 3600;
+                mm = s1 / 60;
+                s2 = s1 % 60;
+             Console.WriteLine("{0} Hours, {1} Minutes, {2} Seconds", hh, mm, s2);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExecuteCommand("shutdown.exe /a");
+            hourLabel.Text = ("00");
+            minuteLabel.Text = ("00");
+            secondLabel.Text = ("00");
+            timer1.Stop();
+            MessageBox.Show("Shutdown Aborted");
+
+            Close();
+        }
+
+        private void thisIsTrialProgramNoWarrentyIdGivenFromDeveloperUseItInYourOwnRiskToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            s2 = s2 - 1;
+            if (s2 == -1)
+            {
+                mm = mm - 1;
+                s2 = 59;
+            }
+            if (mm == -1)
+            {
+                hh = hh - 1;
+                mm = 59;
+            }
+            hourLabel.Text = Convert.ToString(hh).PadLeft(2, '0');
+            minuteLabel.Text = Convert.ToString(mm).PadLeft(2, '0');
+            secondLabel.Text = Convert.ToString(s2).PadLeft(2, '0');
+        }
+
+        
+    }
+}
